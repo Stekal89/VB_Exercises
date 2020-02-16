d1c3525676836486f74c07c75cf090209e5f0151
Thomas Stekovits <t.stekovits@hotmail.com>
2020-02-16 01:25:38 +0100

    #6 01/01_Erstellen Sie ein Programm, dass jeweils eine Variable vom Typ double, Int32 und string und boolean anlegt.
     Erstellen Sie ein Programm, dass jeweils eine Variable vom Typ double, Int32 und string und boolean anlegt. Weisen Sie den Variablen folgende Werte zu: 123.123, "Hallo Welt", 456, false

--- Modul_00/1_Variablen-VB/Program.vb
+++ Modul_00/1_Variablen-VB/Program.vb
@@ -2,6 +2,60 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+
+        Dim nl As String = vbCrLf
+
+        '                           Aufgabenblatt -Variablen
+        '1. Erstellen Sie ein Programm, dass jeweils eine Variable vom Typ double, Int32 und string und
+        'Boolean anlegt.Weisen Sie den Variablen folgende Werte zu:    123.123, "Hallo Welt", 456, false
+
+        Dim dNumb As Double = 123.123
+        Dim dInt As Integer = 456
+        Dim text As String = "Hallo Welt"
+        Dim tf As Boolean = False
+
+
+        Console.WriteLine($"{nl}############# Exercise 1: #############")
+        Console.WriteLine($"Double:  ""{dNumb}""")
+        Console.WriteLine($"Int32:   ""{dInt}""")
+        Console.WriteLine($"String:  ""{text}""")
+        Console.WriteLine($"Boolean: ""{tf}""{nl}{nl}")
+
+        '2. Erstellen Sie ein Programm, das zwei Variablen vom Typ int anlegt und diese auf der Konsole
+        'ausgibt.
+
+
+        '3. Erstellen Sie ein Programm, das drei Variablen vom Typ int mit den Werten x:15, y:76 und z:24
+        'anlegt.Geben Sie die Variablennamen und die Werte nebeneinander auf der Konsole aus.
+
+
+        '4. Berechnen Sie x * y + z Weisen Sie das Ergebnis an eine Variable namens ergebnis zu. Geben Sie
+        'diese Variable und die Rechnungen auf der Konsole aus.
+
+
+        '5. Berechnen Sie (x*x*z)/y + x und z+z-x+y. Verwenden Sie die gleiche Variable wie vorher um das
+        'Ergebnis zu speichern und geben sie nach jedem Schritt die Rechnung und das ergebnis aus.
+
+
+        '6. Erstellen Sie drei Variablen x, y und z vom typ int. Weisen Sie den Variablen beliebige Zahlen zu.
+        'Erstellen Sie eine weitere Variable b. Berechnen Sie folgende Vergleiche: x +y < z, x > y - z, x <> y,
+        'x = z und speichern Sie das Ergebnis in die Variable b. Geben Sie nach jedem Vergleich die
+        'Variable b auf der Konsole aus.
+
+
+        '7. Erstellen Sie zwei String Variablen. Geben Sie die Variablen auf der Konsole untereinander aus.
+        'Geben Sie die Variablen danach auf einer Zeile aus. Benutzen Sie dazu einerseits den '&'
+        'Operator zusammen mit Console.WriteLine und andererseits Console.Write zum Ausgeben.
+
+
+        '8. Führen Sie Aufgabe 5 und 6 mit Variablen, die Sie vom Benutzer einlesen aus.
+
+
+        '9. Erstellen Sie ein Programm, das einen Namen, ein Alter und ein Geburtsdatum von der Konsole
+        'einliest.und Geben Sie folgenden Satz auf der Konsole aus: Hallo 'Name' du bist 'Alter' alt und
+        'am 'Geburtsdaum' geboren
+
+
+        Console.ReadKey()
     End Sub
 End Module
