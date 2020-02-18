f8ca6106c4fb812efef4b28a4184f455e014ae20
Thomas Stekovits <t.stekovits@hotmail.com>
2020-02-18 10:31:45 +0100

    #3 3_Schleifen
    Create Project for exercises "Aufgabenblatt – Schleifen"

---
+++ Modul_00/3_Schleifen/Program.vb
@@ -0,0 +1,77 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        '        Aufgabenblatt -Schleifen
+
+
+#Region "1)"
+
+        ' 1. Erstellen Sie eine While Schleife, die die Zahlen von 0 bis 10 ausgibt.
+
+
+#End Region
+
+#Region "2)"
+
+        ' 2. Erstellen Sie eine For Schleife, die die Zahlen von 0 bis 100 ausgibt.
+
+#End Region
+
+#Region "3)"
+
+        ' 3. Erstellen Sie eine While Schleife, die rückwärts von 100 bis - 10 zählt und die Zahlen ausgibt.
+
+#End Region
+
+#Region "4)"
+
+        ' 4. Erstellen Sie eine While Schleife, die in zweier- Schritten von 0 bis 30 zählt und die Zahlen
+        ' ausgibt.
+
+#End Region
+
+#Region "5)"
+
+        ' 5. Erstellen Sie ein Programm, das zwei Zahlen von 0 bis 10 von der Konsole einliest. Die Schleife
+        ' soll von der ersten eingegebenen Zahl bis zur zweiten eingegebenen Zahl zählen.
+
+#End Region
+
+#Region "6)"
+
+        ' 6. Erstellen Sie ein Programm, das je nach Eingabe eine bestimmte Anzahl von * ausgibt.
+
+#End Region
+
+#Region "7)"
+
+        ' 7. Erstellen Sie ein Programm, das von 0 bis 100 zählt. Das Programm soll bei jeder Zahl überprüfen,
+        ' ob Sie ohne Rest durch 2 Teilbar ist. (Hinweis: Modulo.zahl Mod 2 = 0). Wenn ja, soll das
+        ' Programm 'Zahl' ist durch 2 Teilbar ausgeben. ansonsten 'Zahl' ist nicht durch 2 Teilbar.
+
+#End Region
+
+#Region "8)"
+
+        ' 8. Verbessern Sie das Programm aus Übung 7. Der Benutzer kann jetzt einen Zahlenbereich wählen
+        ' und eine Zahl. Danach soll überprüft werden welche Zahlen in dem Zahlenbereich durch die Zahl
+        ' teilbar sind.
+        ' 9. Entwickeln Sie ein Programm, dass einem Einbrecher hilft ein 4 Stelliges Zahlenschloss zu
+        ' knacken.Das Zahlenschloss akzeptiert an jeder Stelle Zahlen von 0-36. Es besitzt 4 Stellen.
+        ' Eine mögliche Kombination wäre zb.
+        ' 36-22-11-14
+        ' Schreiben Sie eine Funktion, die ihnen alle möglichen Kombinationen des Zahlenschlosses (auf
+        ' der Konsole) ausgibt.
+        ' 10. Erstellen Sie ein Programm, das das kleine Einmaleins ausgibt.11. Geben Sie einen Christbaum aus. Der Benutzer kann die Höhe wählen:
+        ' zb.: Höhe 10
+        ' Die Formel für den Baum ist
+        ' Leerzeichenanzahl Links / Rechts = Höhe – 1 – i
+        ' Sterne = i * 2 + 1
+        ' i = Nummer des Durchlaufs.
+
+#End Region
+
+    End Sub
+End Module
