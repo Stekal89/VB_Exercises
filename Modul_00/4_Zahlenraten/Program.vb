fec282e0ad3ce9613f48a20e35582cd4c31541d5
Thomas Stekovits <t.stekovits@hotmail.com>
2020-02-24 11:18:54 +0100

    #4 4_Zahlenraten -> Project created
    Created Project in the Modul_00 Solution to develop the "Zahlenraten"  application.

---
+++ Modul_00/4_Zahlenraten/Program.vb
@@ -0,0 +1,21 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        'Zahlenraten:
+        'Programmieren Sie ein Spiel, bei dem der Benutzer eine Zufallszahl zwischen 1 und 6 erraten muss.
+        '1. Lesen Sie dazu einen Wert von der Eingabeaufforderung ein.
+        '2. Ermitteln Sie danach einen Wert zwischen 1 und 6 mittels der Funktion Random.Next(1,6)
+        '3. Wandeln Sie die eingegebene Zeichenkette in eine Zahl um ! (int.Parse(..))
+        '4. Vergleichen Sie den eingegebenen Wert mit der eingelesenen Zahl. Sind sie gleich, hat der
+        'Benutzer gewonnen andernfalls verloren. Geben Sie eine enstprechende Nachricht auf der
+        'Kommandozeile aus.
+        '5. Erweitern Sie ihr Programm, sodass es solange weitermacht, bis der Benutzer ein mal
+        'gewonnen hat.
+        '6. Erweitern Sie ihr Programm, sodass der Benutzer 6 mal gewinnen muss bevor das Spiel
+        'gewonnen ist.
+        ' Benutzen Sie Console.Clear() an einer geeigneten Stelle um die alten eingaben zu löschen.
+
+    End Sub
+End Module
