3fd449024f0e2e7a1b1a34bcfaf22530f8db1ad5
Thomas Stekovits <t.stekovits@hotmail.com>
2020-02-24 12:09:44 +0100

    #4 4_Zahlenraten -> Exercise 1 - 4
    1) Read input from console
    2) Validate input
       - Not null, empty, or whitespace
       - Input is an Integer
       - Input value is between 1 and 6
    3) Generate random number using Random.Next() method
    4) Verify if user input and auto-generated number are equal

--- Modul_00/4_Zahlenraten/Program.vb
+++ Modul_00/4_Zahlenraten/Program.vb
@@ -1,21 +1,82 @@
 Imports System
 
 Module Program
+    Dim rnd As Random = New Random
+
     Sub Main(args As String())
+
+        Dim nl As String = vbCrLf
 
         'Zahlenraten:
         'Programmieren Sie ein Spiel, bei dem der Benutzer eine Zufallszahl zwischen 1 und 6 erraten muss.
-        '1. Lesen Sie dazu einen Wert von der Eingabeaufforderung ein.
-        '2. Ermitteln Sie danach einen Wert zwischen 1 und 6 mittels der Funktion Random.Next(1,6)
-        '3. Wandeln Sie die eingegebene Zeichenkette in eine Zahl um ! (int.Parse(..))
-        '4. Vergleichen Sie den eingegebenen Wert mit der eingelesenen Zahl. Sind sie gleich, hat der
-        'Benutzer gewonnen andernfalls verloren. Geben Sie eine enstprechende Nachricht auf der
-        'Kommandozeile aus.
-        '5. Erweitern Sie ihr Programm, sodass es solange weitermacht, bis der Benutzer ein mal
-        'gewonnen hat.
-        '6. Erweitern Sie ihr Programm, sodass der Benutzer 6 mal gewinnen muss bevor das Spiel
-        'gewonnen ist.
-        ' Benutzen Sie Console.Clear() an einer geeigneten Stelle um die alten eingaben zu löschen.
+
+        Dim success As Boolean = False
+
+        Do
+            Dim guesTry As Integer = 1
+
+            Console.WriteLine($"{nl}{nl}{vbTab}# # # # N U M B E R   G U E S S # # # #")
+            '1. Lesen Sie dazu einen Wert von der Eingabeaufforderung ein.
+            Console.Write("Please insert your number (1 - 6): ")
+            Dim userNumbAsString As String = Console.ReadLine()
+            Console.WriteLine()
+
+            If Not String.IsNullOrEmpty(userNumbAsString) And Not String.IsNullOrWhiteSpace(userNumbAsString) Then
+
+                '3. Wandeln Sie die eingegebene Zeichenkette in eine Zahl um ! (int.Parse(..))
+                Dim userNumb As Integer
+                If Integer.TryParse(userNumbAsString, userNumb) Then
+
+                    If userNumb >= 1 And userNumb <= 6 Then
+
+                        '2. Ermitteln Sie danach einen Wert zwischen 1 und 6 mittels der Funktion Random.Next(1,6)
+                        Dim randomNumb As Integer = rnd.Next(1, 6)
+
+                        '4. Vergleichen Sie den eingegebenen Wert mit der eingelesenen Zahl. Sind sie gleich, hat der
+                        'Benutzer gewonnen andernfalls verloren. Geben Sie eine enstprechende Nachricht auf der
+                        'Kommandozeile aus.
+
+                        Console.WriteLine($"Your Number:   ""{userNumb}""")
+                        Console.WriteLine($"Random Number: ""{randomNumb}""")
+
+                        If userNumb = randomNumb Then
+                            Console.ForegroundColor = ConsoleColor.DarkGreen
+                            Console.WriteLine("You win. Numbers are matching!")
+                            Console.ForegroundColor = ConsoleColor.Gray
+                        Else
+                            Console.ForegroundColor = ConsoleColor.Red
+                            Console.WriteLine("You loose. Numbers are not matching!")
+                            Console.ForegroundColor = ConsoleColor.Gray
+                        End If
+
+                        '5. Erweitern Sie ihr Programm, sodass es solange weitermacht, bis der Benutzer ein mal
+                        'gewonnen hat.
+
+                        '6. Erweitern Sie ihr Programm, sodass der Benutzer 6 mal gewinnen muss bevor das Spiel
+                        'gewonnen ist.
+
+                        '7. Benutzen Sie Console.Clear() an einer geeigneten Stelle um die alten eingaben zu löschen.
+
+                    Else
+                        Console.WriteLine("Input is not between 1 and 6!")
+                        Console.WriteLine($"Input: ""{userNumb}""")
+                    End If
+
+                Else
+                    Console.WriteLine("Input is not an Integer!")
+                    Console.WriteLine($"Input: ""{userNumb}""")
+                End If
+
+            Else
+                Console.WriteLine("Input is null, empty, or whitespace!")
+
+            End If
+
+            Console.WriteLine($"{nl}{nl}Continue with any key...")
+            Console.ReadKey()
+            Console.Clear()
+
+        Loop While Not success
 
     End Sub
 End Module
