f72e9227d87b978a32d54ab40758c0916fe95939
Thomas Stekovits <t.stekovits@hotmail.com>
2020-02-25 11:52:57 +0100

    #15 Dual-Converter -> Created functionality to convert Decimal into Binary
    Created functionality to convert Decimal into Binary:
    - Get integer from user
    - add Modulo result into a stack
    - to group the result in groups of four, add missing "0" if needed
    - Add binaries in correct order to the endResult variable (group in groups of four)
    - Display result on the Console

--- Modul_00/5_Dual-Converter/Program.vb
+++ Modul_00/5_Dual-Converter/Program.vb
@@ -1,6 +1,24 @@
 Imports System
 
 Module Program
+
+#Region "Help-Functions"
+    Function ColorRed()
+        Console.ForegroundColor = ConsoleColor.Red
+        Return Nothing
+    End Function
+
+    Function ColorGray()
+        Console.ForegroundColor = ConsoleColor.Gray
+        Return Nothing
+    End Function
+
+    Function ColorDarkGreen()
+        Console.ForegroundColor = ConsoleColor.DarkGreen
+        Return Nothing
+    End Function
+#End Region
+
     Sub Main(args As String())
 
         Dim nl As String = vbCrLf
@@ -24,6 +42,88 @@ Module Program
         '1 * 2^6 + 1 * 2^5 + 0 * 2^4 + 0*2^3 + 1* 2^2 + 0 * 2^1 + 0* 2^0 = 100
         '64 + 32 + 4 = 100
 
+        Dim inputAsString As String
+
+        Do
+            Console.WriteLine($"{nl}{nl}{vbTab}# # # # D U A L - C O N V E R T E R # # # #{nl}")
+
+            Console.WriteLine("To exit the application, please write exit into the command line!")
+            Console.Write("Please insert your number, which you want to convert: ")
+            inputAsString = Console.ReadLine()
+            Console.WriteLine()
+
+            Dim input As Integer? = ConvertStringIntoInteger(inputAsString)
+
+            If input IsNot Nothing Then
+
+                ' Convert to Binary
+                Dim converted As Boolean = False
+                Dim rest As Integer
+                Dim res As Double
+                Dim binList As New Stack(Of Integer)
+
+                Do
+                    ' Get binary result (one index)
+                    rest = input Mod 2
+
+                    ' Get result of the calculation, rounded down, because we only need the first
+                    res = input / 2
+                    res = Math.Floor(res)
+
+                    'Console.WriteLine($"{input} / 2 = {res} ""{rest} Rest""")
+
+                    ' Add result to stack of binaries
+                    binList.Push(rest)
+
+                    ' overwrite the input with the result to calculate the next binary
+                    input = CInt(res)
+                Loop While input > 0
+
+                ' To group the result in groups of four we need to add "0" into the stacks
+                Dim zeroNeeded As Integer = binList.Count Mod 4
+                If zeroNeeded <> 4 Then
+
+                    For i = 1 To 4 - zeroNeeded
+                        binList.Push(0)
+                    Next
+
+                End If
+
+                Dim endResult As String = Nothing
+                For i = 0 To binList.Count() - 1
+
+                    ' Group the result in groups of four
+                    If Not String.IsNullOrEmpty(endResult) Then
+
+                        Dim withoutWhiteSpaces As New String(endResult.Where(Function(x) Not Char.IsWhiteSpace(x)).ToArray())
+
+                        If withoutWhiteSpaces.Count Mod 4 = 0 Then
+                            endResult += " "
+                        End If
+                    End If
+
+                    ' Add result to the variable, which will be needed to display the endresult on the console.
+                    endResult += binList.Pop().ToString()
+                Next
+
+                Console.WriteLine($"{nl}EndResult: ""{endResult}""")
+
+            Else
+                ColorRed()
+                Console.WriteLine("Cannot convert INPUT into INTEGER!")
+                ColorGray()
+
+            End If
+
+            Console.WriteLine($"{nl}{nl}Continue With any key...")
+            Console.ReadKey()
+            Console.Clear()
+
+
+        Loop While inputAsString <> "Exit"
+
+
+
         '2) Gliedern Sie die Berechnungsmethode in eine Funktion aus, die eine int zahl übernimmt und eine
         'Binärzahl (in Form eines Strings) zurückgibt.
 
@@ -42,9 +142,40 @@ Module Program
         'Achtung ! in Visual Basic wird eine ganzzahlige Division mit dem „\“ (Backslash) – Operator
         'durchgeführt.
 
-        Console.Clear()
-        Console.WriteLine($"{nl}{nl}Continue with any key...")
-        Console.ReadKey()
+
 
     End Sub
+
+    ''' <summary>
+    ''' Gets a value as string and validates it:
+    ''' - if string is not null, empty, or whitespace
+    ''' - if input is integer -> TryParse
+    ''' </summary>
+    ''' <param name="input">value as string</param>
+    ''' <returns>success -> string converted as integer
+    '''          failure -> "-1"</returns>
+    Function ConvertStringIntoInteger(input As String) As Integer
+
+        If Not String.IsNullOrEmpty(input) And Not String.IsNullOrWhiteSpace(input) Then
+
+            Dim output As Integer
+            If Integer.TryParse(input, output) Then
+                Return output
+            Else
+                ColorRed()
+                Console.WriteLine("Input is not an Integer!")
+                Console.WriteLine($"Input: ""{input}""")
+                ColorGray()
+            End If
+
+        Else
+            ColorRed()
+            Console.WriteLine("Input is null, empty, or whitespace!")
+            ColorGray()
+        End If
+
+        Return Nothing
+
+    End Function
+
 End Module
